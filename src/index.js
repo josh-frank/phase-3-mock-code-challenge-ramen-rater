c8d970d8d527c35f9c08de4b12c44ba3357be926
Josh Frank <eurosewusa@gmail.com>
2021-02-09 11:45:04 -0500

    fixed bug, core deliverables complete

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-
 const ramenUrl = 'http://localhost:3000/ramens'
 const ramenMenu = document.getElementById("ramen-menu")
 const ramenDetail = document.getElementById("ramen-detail")
@@ -11,7 +10,6 @@ function fetchRamens(){
 }
 
 function renderRamen(ramen){
-
   const thisImage = document.createElement('img')
   thisImage.dataset.id = ramen.id
   thisImage.dataset.name = ramen.name
@@ -21,13 +19,10 @@ function renderRamen(ramen){
   thisImage.src = ramen.image
   thisImage.alt = ramen.name
   ramenMenu.append(thisImage)
-  
 }
 
 function showRamen(ramenClick){
-  
   if(ramenClick.target.tagName === "IMG"){
-
     ramenDetail.querySelector('img').src = ramenClick.target.src
     ramenDetail.querySelector('h2').innerText = ramenClick.target.dataset.name
     ramenDetail.querySelector('h3').innerText = ramenClick.target.dataset.restaurant
@@ -35,40 +30,36 @@ function showRamen(ramenClick){
     ramenRating.elements.rating.value = ramenClick.target.dataset.rating
     ramenRating.elements.comment.value = ramenClick.target.dataset.comment
   }
-
 }
 
-function updateRamen(formSubmition){
- formSubmition.preventDefault()
-
- const ramenToUpdateId = parseInt(ramenDetail.dataset.id)
- const ramenUpdate = {id: ramenToUpdateId, comment: formSubmition.target.elements.comment.value, rating:formSubmition.target.elements.rating.value}
- 
- if(ramenDetail.dataset.id){
-   fetch(`${ramenUrl}/${ramenToUpdateId}`,{
+function updateRamen(formSubmission){
+  formSubmission.preventDefault()
+  const ramenToUpdateId = parseInt(ramenDetail.dataset.id)
+  const ramenUpdate = {
+    id: ramenToUpdateId,
+    comment: formSubmission.target.elements.comment.value,
+    rating:formSubmission.target.elements.rating.value
+  }
+  if(ramenDetail.dataset.id){
+    fetch(`${ramenUrl}/${ramenToUpdateId}`,{
     method:"PATCH",
     headers:{
         'Content-Type':'application/json'
     },
     body: JSON.stringify(ramenUpdate)
-   })
-   .then(response => response.json())
-   .then(data => function(data){
-    // document.querySelector(`img[data-id="${ramenToUpdateId}"]`).dataset.rating = data.rating
-    // document.querySelector(`img[data-id="${ramenToUpdateId}"]`).dataset.comment = data.comment
-    console.log(data)
-   })
- }
-
+    })
+    .then(response => response.json())
+    .then(data => {
+      document.querySelector(`img[data-id="${ramenToUpdateId}"]`).dataset.rating = data.rating
+      document.querySelector(`img[data-id="${ramenToUpdateId}"]`).dataset.comment = data.comment
+    })
+  }
 }
 
-
-
 document.addEventListener('DOMContentLoaded',function(){
-  
-    fetchRamens()
-    ramenMenu.addEventListener('click',showRamen)
-    ramenRating.addEventListener('submit', updateRamen)
+  fetchRamens()
+  ramenMenu.addEventListener('click',showRamen)
+  ramenRating.addEventListener('submit', updateRamen)
 })
 
 
